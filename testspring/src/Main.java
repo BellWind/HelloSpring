c9cd05500c8f1cfdc0c327a148eaa337725da38d
mayu <bellwind@foxmail.com>
2019-06-28 17:41:42 +0800

    spring学习

---
+++ testspring/src/Main.java
@@ -0,0 +1,13 @@
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+    public static void main(String[] args) {
+        //1.创建Sprint的IOC容器的对象
+        ApplicationContext ctx = new ClassPathXmlApplicationContext("applicationContext.xml");
+        //2.从IOC的容器中获取Bean的实例
+        HelloWorld helloWorld = (HelloWorld) ctx.getBean("helloworld");
+        //3.调用hello方法
+        helloWorld.hello();
+    }
+}
